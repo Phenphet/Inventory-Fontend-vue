64a7769183cee9b829501a0cb4047687147a94f0
Phenphet <potpt1098@gmail.com>
2025-05-03 18:00:35 +0700

    add page

---
+++ src/views/CategoryView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <div class="content-header">
+      <div class="container-fluid">
+        <h1>Category</h1>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style></style>
